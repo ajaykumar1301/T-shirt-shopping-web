e3d87504631c420716f0f1b51f233718295b8023
Ajay <ajaykumar1301.0709@gmail.com>
2021-09-15 16:20:57 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-
+import React, {useState} from "react";
 
 function App() {
-  return (<>
-  <Navbar title="textutils" aboutText="About" />
-  <div className="container">
-  <TextForm heading="ENTER YOUR TEXT HERE"/>
 
+const [mode, setMode] = useState("light");
+const toggleMode = ()=>{
+  if(mode === "light"){
+    setMode("dark");
+    document.body.style.backgroundColor='#030c3b';
+  }
+  else{
+    setMode("light");
+    document.body.style.backgroundColor='white';
+
+  }
+}
+  return (
+  <>
+
+  <Navbar title="textutils" aboutText="About"  toggleMode={toggleMode} mode={mode}/>
+
+   <div className="container">
+  <TextForm heading="ENTER YOUR TEXT HERE" mode={mode}/> 
   </div>
- 
+
+  <About/>
+
   </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -18,19 +18,20 @@ export default function TextForm(props) {
 
 
     return (
-        <>
-        <div className="container">
+        <> 
+        <div className="container "  style={{color: props.mode==='dark'?'white':'black'}}>
             <h1>{props.heading}</h1>
             <div class="mb-3">
-            <textarea class="form-control my-2" value={text} onChange={handleUpChange} id="myBox" rows="8"></textarea>
+            <textarea class="form-control my-2" value={text} style={{backgroundColor:props.mode==='light'?'white':'grey',  
+            color:props.mode==='light'?'black':'white'}} onChange={handleUpChange} id="myBox" rows="8"></textarea>
             <button className="btn btn-primary mx-1" onClick={handleUpClick}>Con to UC</button>
             <button className="btn btn-primary mx-1" onClick={handleLoClick}>Con to LC</button>
             </div>
         </div>
-        <div className="container my-3">
+        <div className="container my-3" style={{color:props.mode==='light'?'black':'white'}}>
             <h2>Your Text Here</h2>
             <p>{text.split(" ").length} words and {text.length} characters</p>
-            <h2>Preview</h2>
+            <h2 >Preview</h2>
             <p>{0.08 * text.split(" ").length }</p>
         </div>
         </>
